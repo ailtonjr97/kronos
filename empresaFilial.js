3c60ad352565eff645b6fb282b810592848528be
Ailton <ailton.souza97@hotmail.com>
2023-05-02 13:54:04 -0300

    Separando rotas

---
+++ empresaFilial.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router();
+const dotenv = require("dotenv")
+const axios = require("axios")
+dotenv.config();
+
+router.use((req, res, next) => {
+  console.log('Time: ', Date.now())
+  next()
+})
+
+
+router.use("/empresas", async (req, res, next) => {
+    try {
+      await axios
+        .get(process.env.APITOTVS + "api/framework/environment/v1/companies", {
+          auth: {
+            username: process.env.USER,
+            password: process.env.SENHAPITOTVS,
+          },
+        })
+        .then((response) => {
+          res.send(response.data.items)
+        });
+    } catch (err) {
+      return res.send(
+        "Erro ao retornar lista das empresas Protheus. Tente novamente mais tarde."
+      );
+    }
+  });
+  //-------------------------------------------------------------------------------
+  router.use("/filiais", async (req, res, next) => {
+    try {
+      await axios
+        .get(process.env.APITOTVS + "api/framework/environment/v1/branches", {
+          auth: {
+            username: process.env.USER,
+            password: process.env.SENHAPITOTVS,
+          },
+        })
+        .then((response) => {
+          res.send(response.data.items)
+        });
+    } catch (err) {
+      return res.send(
+        "Erro ao retornar lista das filiais Protheus. Tente novamente mais tarde."
+      );
+    }
+  });
+  //------------------------------------------------------------------------------
+  module.exports = router;
--- index.js
+++ index.js
@@ -1,75 +1,26 @@
-// Import the functions you need from the SDKs you need
-import path from "path";
-import express from "express";
-import { initializeApp } from "firebase/app";
-import bodyParser from "body-parser";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import dotenv from "dotenv"
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv")
+const empresaFilial = require("./empresaFilial.js");
 dotenv.config();
+const app = express();
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: process.env.APIKEY,
-  authDomain: "system-management-kronos.firebaseapp.com",
-  projectId: "system-management-kronos",
-  storageBucket: "system-management-kronos.appspot.com",
-  messagingSenderId: "768497722773",
-  appId: "1:768497722773:web:4e2525ce23d575f61d029c",
-  measurementId: "G-ZM0T7L4S9N"
-};
-
-// Initialize Firebase
-const fire = initializeApp(firebaseConfig);
-const auth = getAuth();
-
-//Express config
-const app = express()
-const port = 3000
-
-//Dirname and body-parser
-const __dirname = path.resolve();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-//Routes
-app.get('/', (req, res) => {
-    res.sendFile('kronos/public/home.html', {root: path.dirname(__dirname)});
-  })
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
 
-  app.get('/register', (req, res) => {
-    res.sendFile('kronos/public/register.html', {root: path.dirname(__dirname)});
-  })
+  next();
+});
 
-app.post('/register', (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      res.redirect('/home')
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // ..
-    });
-})
+app.use("/protheus", empresaFilial);
 
-
-app.get('/menu', (req, res) => {
-  auth.onAuthStateChanged(function(user){
-    if(user){
-      res.sendFile('kronos/public/menu.html', {root: path.dirname(__dirname)});
-    }else{
-      res.redirect("/")
-    }
-  })
-})
-
-
-  app.listen(port, () => {
-    console.log(`Server on port ${port}`)
-  })
+//-------------------------------------------------------------------------------
+app.listen(process.env.PORT, function () {
+  console.log("Servidor Node.js operacional na porta " + process.env.PORT);
+});
